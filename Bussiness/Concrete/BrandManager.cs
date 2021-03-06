d07abeed7e64448443a99fcc38db567a88b99102
mervebozkurt06 <mervebzkrts@gmail.com>
2021-03-06 22:14:28 +0300

    10. Gün Ödev

---
+++ Bussiness/Concrete/BrandManager.cs
@@ -0,0 +1,43 @@
+﻿using Bussiness.Abstract;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bussiness.Concrete
+{
+    public class BrandManager : IBrandService
+    {
+        IBrandDal _brandDal;
+        public BrandManager(IBrandDal brandDal)
+        {
+            _brandDal = brandDal;
+        }
+        public IResult Add(Brand brand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Delete(Brand brand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<List<Brand>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<Brand> GetById(int brandId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Update(Brand brand)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
