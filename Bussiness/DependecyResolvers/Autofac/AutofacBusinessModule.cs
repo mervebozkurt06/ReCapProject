88206d76b7aabd6cea47361ef7781db61645c237
mervebozkurt06 <mervebzkrts@gmail.com>
2021-03-13 19:13:38 +0300

    12. Gün ödev

---
+++ Bussiness/DependecyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,22 @@
+﻿using Autofac;
+using Bussiness.Abstract;
+using Bussiness.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bussiness.DependecyResolvers.Autofac
+{
+    public class AutofacBusinessModule : Module 
+    {
+        //over + space
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<CarManager>().As<ICarService>().SingleInstance(); //IProductService istendiğinde ProductManager instance ı ver
+            builder.RegisterType<EfCarDal>().As<ICarDal>().SingleInstance();
+        }
+    }
+}
